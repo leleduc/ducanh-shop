63409a27b4ce9513b86be01b886635f8f32b5eb2
leleduc <leleduc@gmail.com>
2023-05-09 12:54:48 +0700

    ok

---
+++ ducanh-shop.php
@@ -0,0 +1,15 @@
+/*
+* Plugin Name: Duc Anh Shop
+* Plugin URI: #
+* Description: Duc Anh Shop Plugin.
+* Version: 1.10.3
+* Requires at least: 5.2
+* Requires PHP: 7.2
+* Author: Duc Anh
+* Author URI: https://www.linkedin.com/in/ducanh/
+* License: GPL v2 or later
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+* Update URI: #
+* Text Domain: ducanh-shop
+* Domain Path: /languages
+*/
